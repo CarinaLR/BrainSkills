4dfb74ab31ef29f389ef413f6fed81eb9b303552
CarinaLR <carinalr84@gmail.com>
2020-08-26 09:54:53 -0400

    services style

--- project5/brainSkills/views.py
+++ project5/brainSkills/views.py
@@ -7,13 +7,25 @@ from django.shortcuts import render
 
 load_dotenv()
 
+# Block to render index page
+
 
 def index(request):
     return render(request, "brainSkills/index.html")
 
+# Block to render about our company
+
 
 def about(request):
     return render(request, "brainSkills/about.html")
+
+# block to render services
+
+
+def services(request):
+    return render(request, "brainSkills/services.html")
+
+# Block to render greet
 
 
 def greet(request, name):
