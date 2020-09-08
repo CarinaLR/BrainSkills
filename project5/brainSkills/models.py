595c2e70025ddb23b6506f8fc9bfa3073e729c63
CarinaLR <carinalr84@gmail.com>
2020-09-08 09:48:43 -0400

    add models

--- project5/brainSkills/models.py
+++ project5/brainSkills/models.py
@@ -7,4 +7,54 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    is_guest = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+    price = models.FloatField()
+
+    def _str_(self):
+        return self.name
+
+
+class Level(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+
+    def _str_(self):
+        return self.name
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="service")
+    level = models.ForeignKey(
+        Level, on_delete=models.CASCADE, related_name="level")
+    progress = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Message(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="message")
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self, *args, **kwargs):
+        return self.content
