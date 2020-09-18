b2eebda957501190596345459c3572ebc5e8f11a
CarinaLR <carinalr84@gmail.com>
2020-09-18 14:55:05 -0400

    connect message

--- project5/brainSkills/static/brainSkills/main.js
+++ project5/brainSkills/static/brainSkills/main.js
@@ -1,48 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   //Use user_type button to give a status to user.
-  // document
-  //   .querySelector("#student")
-  //   .addEventListener("click", () => change_user_status("student"));
-  // document
-  //   .querySelector("#teacher")
-  //   .addEventListener("click", () => change_user_status("teacher"));
-  // document
-  //   .querySelector("#guest")
-  //   .addEventListener("click", () => change_user_status("guest"));
-  document.querySelector("#edit_student").onsubmit = () => {
-    console.log("I'm here");
-  };
+  document
+    .querySelector("#w_message")
+    .addEventListener("click", () => write_message(user_id));
 });
 
-function change_user_status(status) {
-  console.log("reach change_user_status");
-  //Change status according to the select button.
-  if (status === student) {
-    console.log("STUDENT");
-    //Load user info
-    fetch("status/student")
-      .then((response) => response.json())
-      .then((users) => {
-        console.log("student - ", users);
-      });
-  }
-  if (status === teacher) {
-    console.log("TEACHER");
-    //Load user info
-    fetch("status/teacher")
-      .then((response) => response.json())
-      .then((users) => {
-        console.log("teacher - ", users);
-      });
-  }
-  if (status === guest) {
-    console.log("GUEST");
-    //Load user info
-    fetch("status/guest")
-      .then((response) => response.json())
-      .then((users) => {
-        console.log("guest - ", users);
-      });
-  }
+function write_message(user_id) {
+  userId = user_id;
+  console.log("reach message - user_id", userId);
   return false;
 }
